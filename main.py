d9af6bf2f5382a41d65c7f56e0826ef200e84b0a
longconan72-collab <longconan72@gmail.com>
2026-06-28 03:03:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "DÁN_TOKEN_MỚI_CỦA_BẠN_VÀO_ĐÂY"
+TOKEN="8232656263:AAELnFMa0WRcHhBF7d8n2pnbGgXS7J0nApw"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Xin chào! Bot HiFami đã hoạt động.")
 
-app = Application.builder().token(TOKEN).build()
+app=Application.builder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
 
 print("Bot đang chạy...")
